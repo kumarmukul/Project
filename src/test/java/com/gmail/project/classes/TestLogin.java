ee49ac82e5e2628bec971c1c0a75e24cf5f79647
Pawan Singh <pawan.singh@gspann.com>
2014-11-19 10:11:14 +0530

    commiting logger

--- src/test/java/com/gmail/project/classes/TestLogin.java
+++ src/test/java/com/gmail/project/classes/TestLogin.java
@@ -1,12 +1,17 @@
 package com.gmail.project.classes;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import com.gmail.project.util.ScreenShot;
+
 public class TestLogin extends BaseTestClass{
 	@Test
-	public void login() throws InterruptedException{
+	public void login() throws InterruptedException, IOException{
 		driver.findElement(By.id("Email")).sendKeys("pawan");
+		ScreenShot.screenshots(driver);
 		Thread.sleep(2000);
 	}
 }
--- src/test/java/com/gmail/project/util/ScreenShot.java
+++ src/test/java/com/gmail/project/util/ScreenShot.java
@@ -1,5 +1,31 @@
 package com.gmail.project.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import com.gargoylesoftware.htmlunit.javascript.host.SimpleArray;
+
 public class ScreenShot {
+	public static void screenshots(WebDriver driver) throws IOException{
+		
+		DateFormat format=new SimpleDateFormat("dd-mm-yyyy_HH_mm_ss");
+		Date d=new Date();
+		String date=format.format(d); 
+		Logs.info(date);
+		Logs.info("//src//test//resources//Screenshots//login"+date.trim().toString()+".png");
+		File srcFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(srcFile,new File(System.getProperty("user.dir")+"//src//test//resources//Screenshots//login"+date.trim().toString()+".png"));
+				
+	}
+	
+	
 
 }
