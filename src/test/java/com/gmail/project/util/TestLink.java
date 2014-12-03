957b4bd2de0ad32ff75e3154b241bd25076ad0dc
Pawan Singh <pawan.singh@gspann.com>
2014-12-03 11:07:07 +0530

    kinoshita

--- src/test/java/com/gmail/project/util/TestLink.java
+++ src/test/java/com/gmail/project/util/TestLink.java
@@ -3,16 +3,12 @@ package com.gmail.project.util;
 import testlink.api.java.client.TestLinkAPIClient;
 import testlink.api.java.client.TestLinkAPIException;
 
-//
-//import testlink.api.java.client.TestLinkAPIClient;
-//import testlink.api.java.client.TestLinkAPIException;
-
 public class TestLink {
 
 	public static String APIkey="09db76469e073b7af5a45a939ecb1332";
 	public static String url="http://localhost/testlink/lib/api/xmlrpc/v1/xmlrpc.php";
 
-	public static void reportResult(String TestProject, String TestPlan, String TestCase, String build, String Notes, String Result) throws TestLinkAPIException{
+	public static void reportResult(String TestProject, String TestPlan, String TestCase, String build, String Notes, String Result) throws TestLinkAPIException, testlink.api.java.client.TestLinkAPIException{
 		TestLinkAPIClient api=new TestLinkAPIClient(APIkey, url);
 		api.reportTestCaseResult(TestProject,TestPlan,TestCase,build,Notes,Result);
 	}
